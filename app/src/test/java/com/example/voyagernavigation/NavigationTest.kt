2b85c88b4b595dce95ce100cdcf4f1f7074b009f
Eliud Githuku <githukueliud@gmail.com>
2023-11-24 20:23:27 +0300

    working on navigation tests

---
+++ app/src/test/java/com/example/voyagernavigation/NavigationTest.kt
@@ -0,0 +1,17 @@
+package com.example.voyagernavigation
+
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+//    @get: Rule
+//    val composeTestRule = createComposeRule<>
+//
+//
+//    @Test
+//    fun navigationTest() {
+//        val navController = NavH
+//    }
+
+
+}
